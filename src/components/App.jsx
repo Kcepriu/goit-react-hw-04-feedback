6724f6f4ee781b2fe6aa2ac7ee9fe94dd6fb9fbc
Kcepriu <kcepriu@gmail.com>
2023-02-12 11:13:49 +0200

    redid to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,45 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 
-class App extends Component {
-  buttonsFeedback = ['good', 'neutral', 'bad'];
+const App = () => {
+  const buttonsFeedback = ['good', 'neutral', 'bad'];
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const hendlerClick = typeButtom => {
+    switch (typeButtom) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+      default:
+        console.log('Error type');
+    }
   };
 
-  hendlerClick = typeButtom => {
-    this.setState(prevState => ({ [typeButtom]: prevState[typeButtom] + 1 }));
-  };
+  return (
+    <div className="App">
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={buttonsFeedback}
+          onLeaveFeedback={hendlerClick}
+        />
+      </Section>
 
-  render() {
-    return (
-      <div className="App">
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.buttonsFeedback}
-            onLeaveFeedback={this.hendlerClick}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          <Statistics
-            goodValue={this.state.good}
-            neutralValue={this.state.neutral}
-            badValue={this.state.bad}
-          />
-        </Section>
-      </div>
-    );
-  }
-}
+      <Section title="Statistics">
+        <Statistics goodValue={good} neutralValue={neutral} badValue={bad} />
+      </Section>
+    </div>
+  );
+};
 
 export default App;
